fdb716d7f5368fbbfa1fe727d2d0dc5ba58763fe
Alvaro-7 <alvaro.7.zurdo@gmail.com>
2025-06-27 15:55:46 +0200

    Primera prueba para verla desplegada en la pagina web

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const boton = document.querySelector('.group');
+const correo = document.getElementById('correo');
 
+boton.addEventListener('mouseenter', () => {
+  correo.style.opacity = '1';
+});
+
+boton.addEventListener('mouseleave', () => {
+  correo.style.opacity = '0';
+});
+
+correo.addEventListener('click', () => {
+  window.location.href = 'mailto:pabloringab@gmail.com';
+});
