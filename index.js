325aed8338ce14f951ab81ce12b567a64d954b6a
Alvaro-7 <alvaro.7.zurdo@gmail.com>
2025-06-28 11:47:45 +0200

    Index responsive

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ boton.addEventListener('mouseleave', () => {
 correo.addEventListener('click', () => {
   window.location.href = 'mailto:pabloringab@gmail.com';
 });
+
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('nav-menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('flex-col'); 
+  });
