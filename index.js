7ae61bfc84a04b21cad09056fb24860e5c06d721
Alvaro-7 <alvaro.7.zurdo@gmail.com>
2025-07-08 00:59:59 +0200

    Sigo mejorando el responsive

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const boton = document.querySelector('.group2');
-const correo = document.getElementById('correo');
-
-boton.addEventListener('mouseenter', () => {
-  correo.classList.remove('opacity-0');
-  correo.classList.add('opacity-100');
-});
-
-boton.addEventListener('mouseleave', () => {
-  correo.classList.remove('opacity-100');
-  correo.classList.add('opacity-0');
-});
-
-correo.addEventListener('click', () => {
-  window.location.href = 'mailto:pabloringab@gmail.com';
-});
-
 const toggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('nav-menu');
 
